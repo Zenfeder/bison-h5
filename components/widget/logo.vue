b450b1612208c6b293c6a8bc27a39c21ac22d29e
CG <1054609489@qq.com>
2018-04-27 10:37:47 +0800

    【新增】通用logo组件

---
+++ components/widget/logo.vue
@@ -0,0 +1,32 @@
+<template>
+  <img class="logo" :class="'logo_' + size" src="~static/logo.png"/>
+</template>
+
+<script>
+export default {
+  name: 'logo',
+  props: {
+    size: {
+      type: String,
+      default: 'md'
+    }
+  }
+}
+</script>
+
+
+<style lang="less" scoped>
+.logo {
+  box-sizing: border-box;
+  border-radius: 100%;
+  &.logo_sm {
+    width: 80/75rem;
+    height: 80/75rem;
+  }
+  &.logo_md {
+    width: 120/75rem;
+    height: 120/75rem;
+  }
+}
+</style>
+
