553bf61113645e98e3ab32544171360ac5f08fa9
CG <1054609489@qq.com>
2018-04-27 18:16:16 +0800

    【修改】jokeItem组件归属修改

--- components/joke/jokeItem.vue
+++ components/joke/jokeItem.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="joke-item-box">
+  <div class="joke-item_box">
     <p>{{ item.content }}</p>
   </div>
 </template>
@@ -14,7 +14,14 @@ export default {
 </script>
 
 <style lang="less" scoped>
-.joke-item-box {
+@import '~assets/style/variable/color.less';
+
+.joke-item_box {
+  box-sizing: border-box;
+  width: 702/75rem;
+  height: 308/75rem;
+  margin: auto;
+  border-bottom: 1/75rem solid @gray-l;
   p {
     font-size: 14/37.5rem;
   }
